b83cc01a6e7f059573729fbf9343a58a24c747a0
Tahara Kazuki <ssp0727.lnc@gmail.com>
2021-11-22 13:58:17 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.info('hello2')
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+  })
+)
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world')
+})
+
+app.listen(8000, () => {
+  console.info('port 8000')
+})
